5bb022cb2b2775c58f1982e72f5c34cf2e33f46f
SKumble <skumble27@gmail.com>
2020-12-18 10:57:23 +1100

    new folders created

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,3 @@
+// Creating a query url to access Geojson Data pertaining to earthquakes that occurred within the last 7 days
+var query_url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
+
